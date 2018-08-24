e235fae035d0e1ae14fd94151ece45964a691c55
Stypox <stypox@pm.me>
2018-08-24 17:07:33 +0200

    Added Texture class.
    Still some work is needed because data() function needs some modern C++ improvements. And other ways to load data may be added.

---
+++ gl-abstractions/texture.cpp
@@ -0,0 +1,89 @@
+#include "texture.h"
+
+namespace stypox::gl {
+	constexpr GLenum channelsToFormat(int nrChannels) {
+		switch (nrChannels) {
+			case 1: return GL_R;
+			case 2: return GL_RG;
+			case 3: return GL_RGB;
+			case 4: return GL_RGBA;
+			default: return 0;
+		}
+	}
+
+	Texture2D::Texture2D(GLint position) :
+	m_position{position} {}
+	Texture2D::Texture2D(GLint position, const Tpath& filename, GLenum saveFormat, GLint detailLevel, bool relativeToDirectory) :
+	m_position{position} {
+		generate();
+		data(filename, saveFormat, detailLevel, relativeToDirectory);
+	}
+	Texture2D::Texture2D(GLint position, const Tpath& filename, GLenum saveFormat, GLenum wrapS, GLenum wrapT, GLenum minFilter, GLenum magFilter, GLint detailLevel, bool relativeToDirectory) :
+	m_position{position} {
+		generate();
+		data(filename, saveFormat, detailLevel, relativeToDirectory);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapS);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+	}
+	Texture2D::~Texture2D() {
+		remove();
+	}
+
+	void Texture2D::generate() {
+		if (m_idGenerated) {
+			glDeleteTextures(1, &m_id);
+		}
+		glGenTextures(1, &m_id);
+		m_idGenerated = true;
+	}
+	void Texture2D::remove() {
+		if (m_idGenerated) {
+			glDeleteTextures(1, &m_id);
+			m_idGenerated = false;
+		}
+	}
+
+	void Texture2D::data(const Tpath& filename, GLenum saveFormat, GLint detailLevel, bool relativeToDirectory) {
+		bind();
+		int width, height, nrChannels;
+		unsigned char* pixels = SOIL_load_image((relativeToDirectory ? m_directory / filename : filename).c_str(), &width, &height, &nrChannels, SOIL_LOAD_AUTO);
+		if (pixels) {
+			m_fileOk = true;
+			glTexImage2D(GL_TEXTURE_2D, detailLevel, saveFormat, width, height, 0, channelsToFormat(nrChannels), GL_UNSIGNED_BYTE, pixels);
+			SOIL_free_image_data(pixels);
+		}
+		else {
+			m_fileOk = false;
+		}
+	}
+	void Texture2D::data(void * pixels, GLenum type, GLsizei width, GLsizei height, GLenum sourceFormat, GLenum saveFormat, GLint detailLevel) {
+		bind();
+		glTexImage2D(GL_TEXTURE_2D, detailLevel, saveFormat, width, height, 0, sourceFormat, type, pixels);
+	}
+	void Texture2D::parameters(GLenum wrapS, GLenum wrapT, GLenum minFilter, GLenum magFilter) {
+		bind();
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapS);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+	}
+
+	void Texture2D::bind() {
+		glActiveTexture(GL_TEXTURE0 + m_position);
+		glBindTexture(GL_TEXTURE_2D, m_id);
+	}
+	void Texture2D::unbind() {
+		glActiveTexture(GL_TEXTURE0 + m_position);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	void Texture2D::bind(GLint position) {
+		glActiveTexture(GL_TEXTURE0 + position);
+		glBindTexture(GL_TEXTURE_2D, m_id);
+	}
+	void Texture2D::unbind(GLint position) {
+		glActiveTexture(GL_TEXTURE0 + position);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+}
---
+++ gl-abstractions/texture.h
@@ -0,0 +1,46 @@
+#ifndef _STYPOX_TEXTURE_H_
+#define _STYPOX_TEXTURE_H_
+
+#include <array>
+#include <GL/glew.h>
+#include <SOIL.h>
+#include <sp/basicTypes.h>
+#include <sp/declarations.h>
+
+namespace stypox::gl {
+	class Texture2D {
+		static Tpath m_directory;
+
+		GLuint m_id;
+		GLint m_position;
+		bool m_idGenerated = false;
+		bool m_fileOk = true;
+	public:
+		Texture2D(GLint position = 0);
+		Texture2D(GLint position, const Tpath& filename, GLenum saveFormat, GLint detailLevel = 0, bool relativeToDirectory = true);
+		Texture2D(GLint position, const Tpath& filename, GLenum saveFormat, GLenum wrapS, GLenum wrapT, GLenum minFilter, GLenum magFilter, GLint detailLevel = 0, bool relativeToDirectory = true);
+		~Texture2D();
+
+		inline GLint position() { return m_position; }
+		inline void setPosition(GLint position) { m_position = position; }
+		inline static Tpath directory() { return m_directory; }
+		inline static void setDirectory(const Tpath& directory) { m_directory = directory; }
+
+		void generate();
+		void remove();
+
+		void data(const Tpath& filename, GLenum saveFormat, GLint detailLevel = 0, bool relativeToDirectory = true);
+		void data(void * data, GLenum type, GLsizei width, GLsizei height, GLenum sourceFormat, GLenum saveFormat, GLint detailLevel = 0);
+		void parameters(GLenum wrapS, GLenum wrapT, GLenum minFilter, GLenum magFilter);
+
+		void bind();
+		void unbind();
+		void bind(GLint position);
+		static void unbind(GLint position);
+	};
+
+	Tpath Texture2D::m_directory {""};
+}
+
+#include "texture.cpp"
+#endif
